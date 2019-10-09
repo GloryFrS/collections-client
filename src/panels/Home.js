2921ec4f79ad58402f2c98d6e7d558fae75a8ad0
GloryFrS <gloryfrs@yandex.ru>
2019-10-09 11:15:28 +0700

    component convas

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
@@ -6,52 +6,8 @@ import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
-import persik from "../img/persik.png"
 
-
-const Home = ({ id, go, fetchedUser }) => {
-	
-	const t = (x,y,size,canvas) => {
-		canvas.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
-		for (let side = 0; side < 7; side++) {	
-			canvas.lineTo(x + size * Math.cos(side * 2 * Math.PI / 6), y + size * Math.sin(side * 2 * Math.PI / 6));
-		}
-
-	}
-	useEffect(() => {
-		let canvas = document.querySelector('#canvas').getContext('2d'),side = 0,
-		size = 100,
-		x = 100,
-		y = 100;
-
-
-		let imageObj = new Image();
-		imageObj.onload = function()
-		{
-			canvas.save();
-			canvas.beginPath();
-			t(100,100,100,canvas);
-			canvas.clip();
-			canvas.drawImage(imageObj, 0, 0);
-		};
-
-		imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
-		
-		canvas.beginPath();
-		canvas.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
-		for (side; side < 7; side++) {
-			canvas.lineTo(x + size * Math.cos(side * 2 * Math.PI / 6), y + size * Math.sin(side * 2 * Math.PI / 6));
-		}
-
-		// canvas.fillStyle = "#fff";
-		canvas.strokeStyle = "gold";
-		canvas.lineWidth = "2"
-		canvas.stroke();
-		// canvas.fill();
-		
-	});
-
-	return (
+const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>Example</PanelHeader>
 		{fetchedUser &&
@@ -66,11 +22,11 @@ const Home = ({ id, go, fetchedUser }) => {
 
 		<Group title="Navigation Example">
 			<Div>
-				<canvas id="canvas" width="400" height="400"></canvas>
+			asd
 			</Div>
 		</Group>
 	</Panel>
-)};
+);
 
 Home.propTypes = {
 	id: PropTypes.string.isRequired,
