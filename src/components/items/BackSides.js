dec5be8976a0f3857b11db1ea6f103308d46ae56
GloryFrS <gloryfrs@yandex.ru>
2019-10-11 11:16:13 +0700

    const and return line add

--- src/components/items/BackSides.js
+++ src/components/items/BackSides.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const BackSides = (props) => {
   const items = [];
+  
   for (let i = 0; i < props.numTimes; i++) {
     items.push(props.children(i));
   }
+  
   return <div className='cards-back-sides'>{items}</div>;
 };
 
