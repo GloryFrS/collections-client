9ddb710f38cdbb36c8786fe7b80491ee26340b9f
GloryFrS <gloryfrs@yandex.ru>
2019-10-14 16:59:47 +0700

    add toolbar pannel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { View } from '@vkontakte/vkui/';
+import { View, TabbarItem, Tabbar, Panel, PanelHeader, Epic } from '@vkontakte/vkui/';
+import boxIco from './img/box-ico.png';
+import collectionIco from './img/collection-ico.png';
+import ratingIco from './img/rating-ico.png';
 import Home from './panels/Home';
 import '@vkontakte/vkui/dist/vkui.css';
 import './styles/main.scss';
@@ -8,23 +11,59 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activePanel: 'home'
+      activeStory: 'box'
     };
+    this.onStoryChange = this.onStoryChange.bind(this);
   }
   
-  go = e => {
-    this.setState({
-      activePanel: e.currentTarget.dataset.to
-    });
-  };
+  onStoryChange = e => {
+    this.setState({ 
+      activeStory: e.currentTarget.dataset.story 
+    })
+  }
+
 
   render() {
-    const { activePanel } = this.state;
+    const { activeStory } = this.state;
 
     return ( 
-      <View activePanel = { activePanel } >
-        <Home id='home' go={ this.go }/> 
-      </View>
+      <Epic activeStory={activeStory} tabbar={
+        <Tabbar>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={activeStory === 'box'}
+            data-story="box"
+            text="Боксы"
+          ><img src={boxIco} alt=''/></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'rating'}
+            data-story="rating"
+            text="Рейтинг"
+          ><img src={collectionIco} alt=''/></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={activeStory === 'collections'}
+            data-story="collections"
+            text="Коллекции"
+          ><img src={ratingIco} alt=''/></TabbarItem>
+        </Tabbar>
+      }>
+        <View id="box" activePanel="box">
+          <Home id='box' go={this.onStoryChange}/>
+        </View>
+        <View id="rating" activePanel="rating">
+          <Panel id="rating">
+            <PanelHeader>Рейтинг</PanelHeader>
+          </Panel>
+        </View>
+        <View id="collections" activePanel="collections">
+          <Panel id="collections">
+            <PanelHeader>Коллекции</PanelHeader>
+          </Panel>
+        </View>
+      </Epic>
+      
     );
   }
 }
