0fdad26c095d9688bec9df821b6725cb8109be12
GloryFrS <gloryfrs@yandex.ru>
2019-10-30 16:46:23 +0700

    Updates

---
+++ precache-manifest.062eb051ededbb3f39b7d5fc7d03cbca.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c199c6c9fd95f50e619d70fcbf8c940",
+    "url": "/collections-client/index.html"
+  },
+  {
+    "revision": "7dfc427829ef9245a864",
+    "url": "/collections-client/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "694b34464d5bf2678374",
+    "url": "/collections-client/static/css/main.44f136d9.chunk.css"
+  },
+  {
+    "revision": "7dfc427829ef9245a864",
+    "url": "/collections-client/static/js/2.8c53f6d6.chunk.js"
+  },
+  {
+    "revision": "694b34464d5bf2678374",
+    "url": "/collections-client/static/js/main.497107a0.chunk.js"
+  },
+  {
+    "revision": "1e2f50271b0178bcbf63",
+    "url": "/collections-client/static/js/runtime-main.af69ed61.js"
+  },
+  {
+    "revision": "b9020a67b8d73bbcf6f9f7539f4af38d",
+    "url": "/collections-client/static/media/b-rang-basic.b9020a67.png"
+  },
+  {
+    "revision": "19880951e586035fb4990284812a295b",
+    "url": "/collections-client/static/media/b-rang-legend.19880951.png"
+  },
+  {
+    "revision": "d60fb267a23845cc865a3ea55a59f0b8",
+    "url": "/collections-client/static/media/b-rang-none.d60fb267.png"
+  },
+  {
+    "revision": "6bfa230a7d4d8e5e952c3c3c4ce8e055",
+    "url": "/collections-client/static/media/b-rang-simple.6bfa230a.png"
+  },
+  {
+    "revision": "ecf0b970a59909747e6d708ba1c58668",
+    "url": "/collections-client/static/media/back-side.ecf0b970.png"
+  },
+  {
+    "revision": "51516e53da82c2bfbd3c31f6cae79b21",
+    "url": "/collections-client/static/media/basic-box.51516e53.png"
+  },
+  {
+    "revision": "433c8e3dc5f55822638ef7bb8ba7ae55",
+    "url": "/collections-client/static/media/chest-cover.433c8e3d.png"
+  },
+  {
+    "revision": "e4eb26b8dfc6fdaeeb686a8e9c5e46c7",
+    "url": "/collections-client/static/media/legend-box.e4eb26b8.png"
+  },
+  {
+    "revision": "360d5d56e20df89cec4f087c3d138932",
+    "url": "/collections-client/static/media/polygon.360d5d56.png"
+  },
+  {
+    "revision": "d0e5d1e6d6a20934ed1e541850d775ca",
+    "url": "/collections-client/static/media/polygon2.d0e5d1e6.png"
+  },
+  {
+    "revision": "191d58b9e00ca13f5a751b6cfb4c970a",
+    "url": "/collections-client/static/media/simple-box.191d58b9.png"
+  },
+  {
+    "revision": "cc61bd6b5975f975320d579289adb862",
+    "url": "/collections-client/static/media/star.cc61bd6b.svg"
+  }
+]);
